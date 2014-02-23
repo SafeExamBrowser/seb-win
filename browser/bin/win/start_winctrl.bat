d474e01ae2bf9511815c6e147085878d45fc3a64
Stefan Schneider <eqsoftdev@gmail.com>
2014-02-23 18:26:59 +0100

    update configs

--- browser/bin/win/start_winctrl.bat
+++ browser/bin/win/start_winctrl.bat
@@ -1,1 +1,1 @@
-..\..\..\..\xulrunner\win\xulrunner\xulrunner.exe -app "..\..\apps\seb.ini" -profile "..\..\data\profile" -config "winctrl" -ctrl 1 -debug 1 -purgecaches -jsconsole
+..\..\..\..\xulrunner\win\xulrunner\xulrunner.exe -app "..\..\apps\seb.ini" -profile "..\..\data\profile" -config "winctrl" -ctrl 1 -language "de-DE" -debug 1 -purgecaches -jsconsole

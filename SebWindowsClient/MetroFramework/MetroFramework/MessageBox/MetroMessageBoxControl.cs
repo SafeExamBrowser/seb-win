ed347dae6fed79d679488ae3591fd0bf61947c50
pawy <git@pawy.net>
2015-07-22 16:04:35 +0200

    removed unused directives

--- SebWindowsClient/MetroFramework/MetroFramework/MessageBox/MetroMessageBoxControl.cs
+++ SebWindowsClient/MetroFramework/MetroFramework/MessageBox/MetroMessageBoxControl.cs
@@ -1,14 +1,8 @@
 ﻿using MetroFramework.Controls;
-using MetroFramework.Drawing;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Drawing;
-using System.Data;
 using System.Diagnostics;
-using System.Text;
 using System.Windows.Forms;
-using MetroFramework.Forms;
 
 namespace MetroFramework
 {

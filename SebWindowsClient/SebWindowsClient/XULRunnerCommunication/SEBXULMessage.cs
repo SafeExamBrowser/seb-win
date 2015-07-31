50a58a0e585380b3d74be10471c761eaac72d542
pawy <git@pawy.net>
2015-07-31 10:31:57 +0200

    Added new Messageformat for XulRunner for Websocket Communication

---
+++ SebWindowsClient/SebWindowsClient/XULRunnerCommunication/SEBXULMessage.cs
@@ -0,0 +1,18 @@
+﻿namespace SebWindowsClient.XULRunnerCommunication
+{
+    public class SEBXULMessage
+    {
+        public enum SEBXULHandler
+        {
+            AdditionalResources
+        }
+
+        public SEBXULMessage(SEBXULHandler handler, dynamic opts = null)
+        {
+            Handler = handler;
+            Opts = opts;
+        }
+        public SEBXULHandler Handler { get; set; }
+        public dynamic Opts { get; set; }
+    }
+}

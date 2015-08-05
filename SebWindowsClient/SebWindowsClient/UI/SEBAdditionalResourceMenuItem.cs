24264affe49accfc6d4b5304098f0591b95bca0a
pawy <git@pawy.net>
2015-08-05 11:05:59 +0200

    AdditionalResources (WIP)

---
+++ SebWindowsClient/SebWindowsClient/UI/SEBAdditionalResourceMenuItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SebWindowsClient.UI
+{
+    public class SEBAdditionalResourceMenuItem : ToolStripMenuItem
+    {
+        public string Identifier { get; set; }
+    }
+}
--- SebWindowsClient/SebWindowsClient/UI/SEBAdditionalResourcesToolStripButton.cs
+++ SebWindowsClient/SebWindowsClient/UI/SEBAdditionalResourcesToolStripButton.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Drawing;
+using System.Drawing.Printing;
 using System.Windows.Forms;
 using System.Windows.Media;
+using MetroFramework;
 using MetroFramework.Controls;
 using SebWindowsClient.ConfigurationUtils;
 using SebWindowsClient.Properties;
 using SebWindowsClient.XULRunnerCommunication;
+using Color = System.Drawing.Color;
 using ListObj = System.Collections.Generic.List<object>;
 using DictObj = System.Collections.Generic.Dictionary<string, object>;
 
@@ -14,12 +17,12 @@ namespace SebWindowsClient.UI
 {
     public class SEBAdditionalResourcesToolStripButton : SEBToolStripButton
     {
-        private ContextMenu menu;
+        private ContextMenuStrip menu;
         public SEBAdditionalResourcesToolStripButton()
         {
             InitializeComponent();
 
-            menu = new ContextMenu();
+            menu = new ContextMenuStrip();
 
             LoadItems();
         }
@@ -33,37 +36,42 @@ namespace SebWindowsClient.UI
         {
             foreach (DictObj l0resource in ((ListObj)SEBSettings.settingsCurrent[SEBSettings.KeyAdditionalResources]))
             {
-                var l0item = new MenuItem();
+                var l0item = new SEBAdditionalResourceMenuItem();
+
                 l0item.Text = l0resource[SEBSettings.KeyAdditionalResourcesTitle].ToString();
-                l0item.Tag = l0resource;
+                l0item.Identifier = l0resource[SEBSettings.KeyIdentifier].ToString();
                 l0item.Click += OnItemClicked;
+                l0item.Image = Resources.closewindow;
 
                 foreach (DictObj l1resource in ((ListObj)l0resource[SEBSettings.KeyAdditionalResources]))
                 {
-                    var l1item = new MenuItem();
+                    var l1item = new SEBAdditionalResourceMenuItem();
                     l1item.Text = l1resource[SEBSettings.KeyAdditionalResourcesTitle].ToString();
-                    l1item.Tag = l1resource;
+                    l1item.Identifier = l1resource[SEBSettings.KeyIdentifier].ToString();
                     l1item.Click += OnItemClicked;
 
                     foreach (DictObj l2resource in ((ListObj)l1resource[SEBSettings.KeyAdditionalResources]))
                     {
-                        var l2item = new MenuItem();
+                        var l2item = new SEBAdditionalResourceMenuItem();
                         l2item.Text = l2resource[SEBSettings.KeyAdditionalResourcesTitle].ToString();
-                        l2item.Tag = l2resource;
+                        l2item.Identifier = l2resource[SEBSettings.KeyIdentifier].ToString();
                         l2item.Click += OnItemClicked;
-                        l1item.MenuItems.Add(l2item);
+                        l1item.DropDownItems.Add(l2item);
                     }
-                    l0item.MenuItems.Add(l1item);
+                    l0item.DropDownItems.Add(l1item);
                 }
-                menu.MenuItems.Add(l0item);
+                menu.Items.Add(l0item);
             }
         }
 
         private void OnItemClicked(object sender, EventArgs e)
         {
-            var item = sender as MenuItem;
-            var resource = item.Tag as DictObj;
-            SEBXULRunnerWebSocketServer.SendMessage(new SEBXULMessage(SEBXULMessage.SEBXULHandler.AdditionalResources,new { id = resource[SEBSettings.KeyIdentifier] }));
+            var item = sender as SEBAdditionalResourceMenuItem;
+
+            if (item != null && item.Identifier != null)
+            {
+                SEBXULRunnerWebSocketServer.SendMessage(new SEBXULMessage(SEBXULMessage.SEBXULHandler.AdditionalResources, new { id = item.Identifier }));
+            }
         }
 
         protected override void OnClick(EventArgs e)

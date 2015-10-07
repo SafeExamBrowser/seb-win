848f94d8e9695251828c6d8473e25f4d7a888612
danschlet <schneider@let.ethz.ch>
2015-10-07 14:07:08 +0200

    Updated version numbers in user agent strings.

--- SebWindowsClient/SebWindowsClient/ConfigurationUtils/SEBClientInfo.cs
+++ SebWindowsClient/SebWindowsClient/ConfigurationUtils/SEBClientInfo.cs
@@ -121,9 +121,9 @@ namespace SebWindowsClient.ConfigurationUtils
         private const string SEB_BROWSER_DIRECTORY  = "SebWindowsBrowser";
         private const string XUL_RUNNER_DIRECTORY   = "xulrunner";
         private const string XUL_SEB_DIRECTORY      = "xul_seb";
-        public const string BROWSER_USERAGENT_DESKTOP = "Mozilla/5.0 (Windows NT 6.3; rv:38.0) Gecko/20100101 Firefox/38.0";
-        public const string BROWSER_USERAGENT_TOUCH = "Mozilla/5.0 (Windows NT 6.3; rv:38.0; Touch) Gecko/20100101 Firefox/38.0";
-        public const string BROWSER_USERAGENT_TOUCH_IPAD = "Mozilla/5.0 (iPad; CPU OS 8_1 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12B410 Safari/600.1.4";
+        public const string BROWSER_USERAGENT_DESKTOP = "Mozilla/5.0 (Windows NT 6.3; rv:41.0) Gecko/20100101 Firefox/41";
+        public const string BROWSER_USERAGENT_TOUCH = "Mozilla/5.0 (Windows NT 6.3; rv:41.0; Touch) Gecko/20100101 Firefox/41";
+        public const string BROWSER_USERAGENT_TOUCH_IPAD = "Mozilla/5.0 (iPad; CPU OS 9_0_2 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13A452 Safari/601.1";
         public const string BROWSER_USERAGENT_SEB = "SEB";
 
         public  const string END_OF_STRING_KEYWORD   = "---SEB---";

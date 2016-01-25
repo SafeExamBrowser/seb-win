5171b952c64980565c0e67fe2f551be3dc7b4daf
pawy <git@pawy.net>
2016-01-25 19:40:59 +0100

    Bugfix: Fix switching between WiFi networks in the WiFi widgets sometimes throws exception.

--- SebWindowsClient/SebWindowsClient/WlanUtils/SEBWlanNetworkSelector.cs
+++ SebWindowsClient/SebWindowsClient/WlanUtils/SEBWlanNetworkSelector.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Input;
+using Microsoft.VisualBasic.Devices;
+using SebWindowsClient.ConfigurationUtils;
 using SebWindowsClient.DiagnosticsUtils;
 using SebWindowsClient.UI;
 
@@ -33,7 +35,7 @@ namespace SebWindowsClient.WlanUtils
             catch (Exception ex)
             {
                 listNetworks.Enabled = false;
-                listNetworks.Items.Add("No network interface found");
+                listNetworks.Items.Add(SEBUIStrings.WlanNoNetworkInterfaceFound);
                 Logger.AddError("No Network interface found",this,ex);
             }
         }
@@ -48,10 +50,23 @@ namespace SebWindowsClient.WlanUtils
                 availableNetworks = wlanInterface.GetAvailableNetworkList(Wlan.WlanGetAvailableNetworkFlags.IncludeAllAdhocProfiles).Where(network => !String.IsNullOrWhiteSpace(network.profileName)).ToList();
                 if (availableNetworks.Count() > 0)
                 {
-                    listNetworks.DataSource =
-                        availableNetworks.Select(network =>
-                                String.Format("{0} Strength: {1}", network.dot11Ssid.GetSSID(), network.wlanSignalQuality))
-                                .ToList();
+                    for (int i = 0; i < availableNetworks.Count; i++)
+                    {
+                        var network = availableNetworks[i];
+                        listNetworks.Items.Add(String.Format("{0} ({1})", network.dot11Ssid.GetSSID(),
+                            network.wlanSignalQuality));
+                        try
+                        {
+                            if (wlanInterface.CurrentConnection.profileName == network.profileName)
+                            {
+                                listNetworks.SelectedIndex = i;
+                            }
+                        }
+                        catch (Exception)
+                        {
+                            //cannot determine currently connected wlan network
+                        }
+                    }
                 }
                 else
                 {
@@ -61,9 +76,9 @@ namespace SebWindowsClient.WlanUtils
             catch (Exception ex)
             {
                 listNetworks.Enabled = false;
-                listNetworks.Items.Add("No known networks found!");
-                listNetworks.Items.Add("you can only connect to networks");
-                listNetworks.Items.Add("that you have used before.");
+                listNetworks.Items.Add(SEBUIStrings.WlanNoNetworksFound);
+                listNetworks.Items.Add(SEBUIStrings.WlanYouCanOnlyConnectToNetworks);
+                listNetworks.Items.Add(SEBUIStrings.WlanThatYouHaveUsedBefore);
                 Logger.AddError("No Networks found", this, ex);
             }
         }
@@ -72,15 +87,23 @@ namespace SebWindowsClient.WlanUtils
         {
             var profileName = availableNetworks[listNetworks.SelectedIndex].profileName;
 
-            buttonConnect.Text = "Connecting...";
+            buttonConnect.Text = SEBUIStrings.WlanConnecting;
             buttonConnect.Enabled = false;
             listNetworks.Enabled = false;
 
-            var profile = wlanInterface.GetProfileXml(profileName);
-            wlanInterface.SetProfile(Wlan.WlanProfileFlags.AllUser, profile, true);
-            wlanInterface.Connect(Wlan.WlanConnectionMode.Profile, Wlan.Dot11BssType.Any, profileName);
-
-            this.Close();
+            try
+            {
+                var profile = wlanInterface.GetProfileXml(profileName);
+                wlanInterface.SetProfile(Wlan.WlanProfileFlags.AllUser, profile, true);
+                wlanInterface.Connect(Wlan.WlanConnectionMode.Profile, Wlan.Dot11BssType.Any, profileName);
+                this.Close();
+            }
+            catch(Exception ex)
+            {
+                SEBMessageBox.Show(SEBUIStrings.WlanConnectionFailedMessageTitle,
+                    SEBUIStrings.WlanConnectionFailedMessage, MessageBoxIcon.Error, MessageBoxButtons.OK);
+                Logger.AddError(ex.Message, this, ex);
+            }
         }
 
         private void listNetworks_SelectedIndexChanged(object sender, EventArgs e)

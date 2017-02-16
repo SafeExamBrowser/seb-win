8a59dcf230388f0446a0a3918b74deb6d2a2447d
Daniel Schneider <daniel.schneider@let.ethz.ch>
2017-02-16 23:37:28 +0100

    Added new SEB style icon to about SEB splash screen.

--- SebWindowsClient/SebWindowsClient/SEBSplashScreen.Designer.cs
+++ SebWindowsClient/SebWindowsClient/SEBSplashScreen.Designer.cs
@@ -55,7 +55,7 @@
             this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Image = global::SebWindowsClient.Properties.Resources.AboutSEB;
             this.pictureBox1.Location = new System.Drawing.Point(0, -4);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(594, 324);

a88393a20e9383b9e7cb1af3ed66130ff924df20
Daniel Schneider <daniel.schneider@let.ethz.ch>
2017-03-20 19:10:16 +0100

    Updated version number to 2.1.5.

--- SebWindowsClient/SebWindowsClient/Properties/AssemblyInfo.cs
+++ SebWindowsClient/SebWindowsClient/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("2.1.*")]
-[assembly: AssemblyInformationalVersion("2.1.4")]
+[assembly: AssemblyInformationalVersion("2.1.5")]

3a17b34857ed4b02e2a2ff79c3b2ea561888c33f
Damian Büchel <damian.buechel@let.ethz.ch>
2017-06-20 15:04:39 +0200

    Removed message box displayed when cleaning of temporary directory failed.

--- SebWindowsClient/SebWindowsClient/ConfigurationUtils/FileCompressor.cs
+++ SebWindowsClient/SebWindowsClient/ConfigurationUtils/FileCompressor.cs
@@ -4,120 +4,120 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
-using System.Windows.Forms;
 using Ionic.Zip;
+using SebWindowsClient.DiagnosticsUtils;
 
 namespace SebWindowsClient.ConfigurationUtils
 {
-    public class FileCompressor : IFileCompressor
-    {
-        private static readonly string TempDirectory = SEBClientInfo.SebClientSettingsAppDataDirectory + "temp\\";
-        private static readonly string TempIconFilename = SEBClientInfo.SebClientSettingsAppDataDirectory + "temp\\tempIcon.png";
+	public class FileCompressor : IFileCompressor
+	{
+		private static readonly string TempDirectory = SEBClientInfo.SebClientSettingsAppDataDirectory + "temp\\";
+		private static readonly string TempIconFilename = SEBClientInfo.SebClientSettingsAppDataDirectory + "temp\\tempIcon.png";
 
-        public static void CleanupTempDirectory()
-        {
-            try
-            {
-                if (Directory.Exists(TempDirectory))
-                {
-                    Directory.Delete(TempDirectory, true);
-                }
-            }
-            catch (Exception)
-            {
-                SEBMessageBox.Show("Could not cleanup temp directory", "Could not cleanup temp directory",
-                    MessageBoxIcon.Error, MessageBoxButtons.OK);
-            }
-        }
-        public string CompressAndEncodeFile(string filename)
-        {
-            var zip = new ZipFile();
-            zip.AddFile(filename,"");
-            var stream = new MemoryStream();
-            zip.Save(stream);
-            return base64_encode(stream.ToArray());
-        }
+		public static void CleanupTempDirectory()
+		{
+			try
+			{
+				if (Directory.Exists(TempDirectory))
+				{
+					Directory.Delete(TempDirectory, true);
+				}
+			}
+			catch (Exception e)
+			{
+				Logger.AddError("Error when trying to delete temporary directory: ", null, e);
+			}
+		}
 
-        public string CompressAndEncodeIcon(Icon icon)
-        {
-            //Save the file first locally
-            icon.ToBitmap().Save(TempIconFilename, ImageFormat.Png);
+		public string CompressAndEncodeFile(string filename)
+		{
+			var zip = new ZipFile();
+			zip.AddFile(filename,"");
+			var stream = new MemoryStream();
+			zip.Save(stream);
+			return base64_encode(stream.ToArray());
+		}
 
-            return CompressAndEncodeFile(TempIconFilename);
-        }
+		public string CompressAndEncodeIcon(Icon icon)
+		{
+			//Save the file first locally
+			icon.ToBitmap().Save(TempIconFilename, ImageFormat.Png);
 
-        public string CompressAndEncodeFavicon(Uri uri)
-        {
-            if (File.Exists(TempIconFilename))
-            {
-                File.Delete(TempIconFilename);
-            }
-            if (!Directory.Exists(TempDirectory))
-            {
-                Directory.CreateDirectory(TempDirectory);
-            }
+			return CompressAndEncodeFile(TempIconFilename);
+		}
 
-            var client = new System.Net.WebClient();
-            client.DownloadFile(
-                string.Format(@"http://www.google.com/s2/favicons?domain_url={0}", uri.Host),
-                TempIconFilename);
-            return CompressAndEncodeFile(TempIconFilename);
-        }
+		public string CompressAndEncodeFavicon(Uri uri)
+		{
+			if (File.Exists(TempIconFilename))
+			{
+				File.Delete(TempIconFilename);
+			}
+			if (!Directory.Exists(TempDirectory))
+			{
+				Directory.CreateDirectory(TempDirectory);
+			}
 
-        public string CompressAndEncodeDirectory(string path, out List<string> containingFilenames)
-        {
-            var zip = new ZipFile();
-            zip.AddDirectory(path, "");
-            var stream = new MemoryStream();
-            zip.Save(stream);
-            containingFilenames = zip.Entries.Select(x => x.FileName.Replace(path, "")).ToList();
-            return base64_encode(stream.ToArray());
-        }
+			var client = new System.Net.WebClient();
+			client.DownloadFile(
+				string.Format(@"http://www.google.com/s2/favicons?domain_url={0}", uri.Host),
+				TempIconFilename);
+			return CompressAndEncodeFile(TempIconFilename);
+		}
 
-        /// <summary>
-        /// Saves the file to a temporary directory and returns the path to the file (without filename)
-        /// </summary>
-        /// <param name="base64">the encoded and compressed file content</param>
-        /// <param name="filename">the filename of the file to save</param>
-        /// <param name="directoryName">the subdirectory of the tempdir (usually the id of the additional resource</param>
-        /// <returns></returns>
-        public string DecompressDecodeAndSaveFile(string base64, string filename, string directoryName)
-        {
-            string tempPath = TempDirectory + directoryName + "\\";
-            if (Directory.Exists(tempPath))
-            {
-                return tempPath;
-            }
-            Directory.CreateDirectory(tempPath);
+		public string CompressAndEncodeDirectory(string path, out List<string> containingFilenames)
+		{
+			var zip = new ZipFile();
+			zip.AddDirectory(path, "");
+			var stream = new MemoryStream();
+			zip.Save(stream);
+			containingFilenames = zip.Entries.Select(x => x.FileName.Replace(path, "")).ToList();
+			return base64_encode(stream.ToArray());
+		}
 
-            var data = base64_decode(base64);
-            var stream = new MemoryStream(data);
-            var zip = ZipFile.Read(stream);
-            zip.ExtractAll(tempPath);
+		/// <summary>
+		/// Saves the file to a temporary directory and returns the path to the file (without filename)
+		/// </summary>
+		/// <param name="base64">the encoded and compressed file content</param>
+		/// <param name="filename">the filename of the file to save</param>
+		/// <param name="directoryName">the subdirectory of the tempdir (usually the id of the additional resource</param>
+		/// <returns></returns>
+		public string DecompressDecodeAndSaveFile(string base64, string filename, string directoryName)
+		{
+			string tempPath = TempDirectory + directoryName + "\\";
+			if (Directory.Exists(tempPath))
+			{
+				return tempPath;
+			}
+			Directory.CreateDirectory(tempPath);
 
-            return tempPath;
-        }
+			var data = base64_decode(base64);
+			var stream = new MemoryStream(data);
+			var zip = ZipFile.Read(stream);
+			zip.ExtractAll(tempPath);
 
-        public MemoryStream DeCompressAndDecode(string base64)
-        {
-            var data = base64_decode(base64);
-            var zipStream = new MemoryStream(data);
-            var zip = ZipFile.Read(zipStream);
-            var stream = new MemoryStream();
-            zip.Entries.First().Extract(stream);
-            return stream;
-        }
+			return tempPath;
+		}
 
-        private string base64_encode(byte[] data)
-        {
-            if (data == null)
-                throw new ArgumentNullException("data");
-            return Convert.ToBase64String(data);
-        }
-        private byte[] base64_decode(string encodedData)
-        {
-            byte[] encodedDataAsBytes = Convert.FromBase64String(encodedData);
-            return encodedDataAsBytes;
-        }
-    }
+		public MemoryStream DeCompressAndDecode(string base64)
+		{
+			var data = base64_decode(base64);
+			var zipStream = new MemoryStream(data);
+			var zip = ZipFile.Read(zipStream);
+			var stream = new MemoryStream();
+			zip.Entries.First().Extract(stream);
+			return stream;
+		}
+
+		private string base64_encode(byte[] data)
+		{
+			if (data == null)
+				throw new ArgumentNullException("data");
+			return Convert.ToBase64String(data);
+		}
+		private byte[] base64_decode(string encodedData)
+		{
+			byte[] encodedDataAsBytes = Convert.FromBase64String(encodedData);
+			return encodedDataAsBytes;
+		}
+	}
 }

ae8990804361c750d2a6d160616af51e70eff99e
dbuechel <damian.buechel@let.ethz.ch>
2017-09-27 15:26:34 +0200

    SEBWIN-177: Merged change from 2.1.7, see https://github.com/SafeExamBrowser/seb-win/commit/b99a9ae7ae3726c458651ba13bbdce6833603be3

---
+++ SebWindowsClient/SebWindowsClient/ProcessUtils/ExecutableInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SebWindowsClient.ProcessUtils
+{
+	public class ExecutableInfo
+	{
+		public string Name { get; }
+		public string OriginalName { get; }
+
+		public ExecutableInfo(string name, string originalName = null)
+		{
+			Name = name ?? string.Empty;
+			OriginalName = originalName ?? string.Empty;
+		}
+
+		public bool HasName
+		{
+			get { return !String.IsNullOrEmpty(Name); }
+		}
+
+		public bool HasOriginalName
+		{
+			get { return !String.IsNullOrEmpty(OriginalName); }
+		}
+	}
+}

4c58f9beaef21f0efc5db38acf376cdf82655580
dbuechel <damian.buechel@let.ethz.ch>
2018-08-29 12:27:21 +0200

    SEBWIN-279: Found acceptable solution to prevent automatic registry reset by service while SEB is running.

--- SebWindowsServiceWCF/SebWindowsServiceWCF.cs
+++ SebWindowsServiceWCF/SebWindowsServiceWCF.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Diagnostics;
 using System.ServiceModel;
 using System.ServiceProcess;
+using System.Threading;
+using System.Threading.Tasks;
 using SEBWindowsServiceContracts;
 using SebWindowsServiceWCF.ServiceImplementations;
 
@@ -46,7 +49,7 @@ namespace SebWindowsServiceWCF
 			try
 			{
 				host.Open();
-				Reset();
+				Task.Run(new Action(ResetOnStartup));
 			}
 			catch (Exception ex)
 			{
@@ -71,12 +74,67 @@ namespace SebWindowsServiceWCF
 			Logger.Log("--- STOPPED SEB SERVICE ---" + Environment.NewLine);
 		}
 
-		private void Reset()
+		private void ResetOnStartup()
+		{
+			const int MAX_ATTEMPTS = 10;
+			const int TEN_SECONDS = 10000;
+			const int THIRTY_SECONDS = 30000;
+
+			Logger.Log($"Waiting {TEN_SECONDS / 1000.0}s before attempting to reset any changes...");
+			Thread.Sleep(TEN_SECONDS);
+
+			for (var attempt = 1; attempt < MAX_ATTEMPTS + 1; attempt++)
+			{
+				if (IsSebRunning())
+				{
+					Logger.Log("Detected new SafeExamBrowser process! Stopping reset attempts.");
+
+					break;
+				}
+
+				var success = Reset();
+
+				if (success)
+				{
+					break;
+				}
+				else if (attempt < MAX_ATTEMPTS)
+				{
+					Logger.Log($"Attempt {attempt}/{MAX_ATTEMPTS} failed, waiting {THIRTY_SECONDS / 1000.0}s before trying again...");
+					Thread.Sleep(THIRTY_SECONDS);
+				}
+				else
+				{
+					Logger.Log($"All {MAX_ATTEMPTS} attempts failed.");
+				}
+			}
+		}
+
+		private bool Reset()
 		{
 			var service = new RegistryService();
 			var success = service.Reset();
 
 			Logger.Log(success ? "Attempt to reset registry was successful." : "Failed to reset all registry changes!");
+
+			return success;
+		}
+
+		private bool IsSebRunning()
+		{
+			try
+			{
+				var processes = Process.GetProcessesByName("safeexambrowser");
+				var isRunning = processes.Length != 0;
+
+				return isRunning;
+			}
+			catch (Exception e)
+			{
+				Logger.Log(e, "Failed to check whether SEB is running - assuming it is...");
+
+				return true;
+			}
 		}
 	}
 }
